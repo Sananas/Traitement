f31b2e47a6cb20a984778647b980a2d35f38ce40
gtsi <gtsi@localhost.localdomain>
2014-11-25 17:26:42 +0100

    Projet de Traitement d'image version 3.1

--- projet/imagealgorithm.cpp
+++
@@ -1,5 +0,0 @@
-#include <algorithm>
-
-#include "imagealgorithm.h"
-
-
--- projet/imagealgorithm.h
+++
@@ -1,7 +0,0 @@
-#ifndef IMAGEALGORITHM_H
-#define IMAGEALGORITHM_H
-
-#include "histogram.h"
-#include  "utility.h"
-
-#endif // IMAGEALGORITHM_H
